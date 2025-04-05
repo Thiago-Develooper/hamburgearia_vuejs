27d2e3cd445af6269cbb0b13b3d48d9ce3fe2c98
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-04-05 15:48:21 -0300

    feat: varias coisas feitas, pedidos component, delete functions em andamento, status complete

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,42 +1,84 @@
 <template>
-    <div id="burger-table">
-        <div>
-            <div id="burger-table-heading">
-                <div class="order-id">#:</div>
-                <div>Cliente:</div>
-                <div>Pão:</div>
-                <div>Carne:</div>
-                <div>Opcionais:</div>
-                <div>Ações:</div>
-            </div>
+    <div id="burger-table" v-if="burgers">
+      <div>
+        <div id="burger-table-heading">
+          <div class="order-id">#:</div>
+          <div>Cliente:</div>
+          <div>Pão:</div>
+          <div>Carne:</div>
+          <div>Opcionais:</div>
+          <div>Ações:</div>
         </div>
-        <div id="burger-table-rows">
-            <div class="burger-trable-rows">
-                <div class="order-number">
-                    <div>João</div>
-                    <div>Pão de trigo</div>
-                    <div>Maminha</div>
-                    <div>
-                        <ul>
-                            <li>Salame</li>
-                            <li>Tomate</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <select name="" class="status">
-                            <option value="">Selecione o status   do hamburguer</option>
-                        </select>
-                        <button class="delete-btn">Cancelar</button>
-                    </div>
-                </div>
-            </div>
+      </div>
+      <div id="burger-table-rows">
+        <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+          <div class="order-number">{{ burger.id }}</div>
+          <div>{{ burger.nome }}</div>
+          <div>{{ burger.pao }}</div>
+          <div>{{ burger.carne }}</div>
+          <div>
+            <ul v-for="(opcional, index) in burger.opcionais" :key="index">
+              <li>{{ opcional }}</li>
+            </ul>
+          </div>
+          <div>
+            <select name="status" class="status" @change="updateBurger($event, burger.id)">
+              <option :value="s.tipo" v-for="s in status" :key="s.id" :selected="burger.status == s.tipo">
+                {{ s.tipo }}
+              </option>
+            </select>
+            <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
+          </div>
         </div>
+      </div>
     </div>
-</template>
+    <div v-else>
+      <h2>Não há pedidos no momento!</h2>
+    </div>
+  </template>
 
 <script>
     export default {
-        name: "Dashboard"
+        name: "Dashboard",
+        data() {
+            return {
+                burgers: null,
+                burger_id: null,
+                status: []
+            }
+        },
+        methods: {
+            async getPedidos() {
+                const req = await fetch("http://localhost:3000/burgers");
+
+                const data = await req.json();
+
+                this.burgers = data;
+                // resgatar estados:
+                this.getStatus()
+            },
+            async getStatus() {
+                const req = await fetch("http://localhost:3000/status")
+
+                const data = await req.json();
+
+                this.status = data;
+
+
+            },
+            async deleteBurger(id) {
+                const req = fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                })
+
+                const res = await req.json();
+
+                this.getPedidos()
+            }
+        },
+        mounted() {
+            this.getPedidos()
+        }
     }
 </script>
 
