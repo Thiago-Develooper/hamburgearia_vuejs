5c896b89f4cc79982a41d3df5225e8584d9e7e80
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-04-05 15:14:19 -0300

    feat(dashboard.vue): structure Dasboard.vue created

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Dashboard component</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Dashboard"
+    }
+</script>
+
+<script>
+
+</script>
--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="main-container">
     <h1>PEDIDOS</h1>
+    <Dashboard />
   </div>
 </template>
+
+<script>
+  export default {
+    name: "Pedidos"
+  }
+</script>
