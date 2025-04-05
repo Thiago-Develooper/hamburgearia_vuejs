c8cfe5609404fde3a961c0764a63f57977a9533a
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-04-05 13:27:55 -0300

    feat(burgerform): limpar campos ao enviar amburger

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -66,7 +66,38 @@ export default {
             this.paes = data.paes
             this.carnes = data.carnes
             this.opcionaisdata = data.opcionais
-        }
+        },
+        async createBurger(e) {
+            e.preventDefault();
+            
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+
+            const dataJson = JSON.stringify(data);
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" }
+            })
+
+            const res = await req.json()
+
+            // colocar mensagem no sistema
+
+            // limpar mensagem no sistema
+
+            // limpar campos
+            this.nome = ""
+            this.carne = ""
+            this.pao = ""
+            this.opcionais = ""
+        },
+
     },
     mounted() {
         this.getIngredients()
