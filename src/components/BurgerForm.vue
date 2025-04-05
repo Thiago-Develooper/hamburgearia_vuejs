9a7f780cd5eeb083f1956128b7f097a70ed74380
Thiago Menezes <thiagoprogramador70x7@gmail.com>
2025-04-05 09:36:38 -0300

    Componente BurgerForm criado e adicionado em HomeView

---
+++ src/components/BurgerForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>BurgerForm component</p>
+</template>
+
+<script>
+export default {
+    name: "BurgerForm"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,19 @@
   <Banner />
   <div class="main-container">
     <h1>Monte o seu burger:</h1>
-    <p>formulário</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import BurgerForm from "@/components/BurgerForm.vue";
 import Banner from "../components/Banner.vue";
 
 export default {
   name: "Home",
   components: {
     Banner,
+    BurgerForm
   },
 };
 </script>
